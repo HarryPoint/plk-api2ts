c85c64e5d29f9ed2b0428ddbaf6035ff7be127e7
梁怀刚 <1751826104@qq.com>
2024-02-28 19:44:32 +0800

    feat: 更新代码

--- src/createTs.ts
+++ src/createTs.ts
@@ -10,13 +10,11 @@ const readFiles = async (config: IConfig, dir: string, project: Project) => {
     const filePath = path.join(dir, file);
     const stat = fs.statSync(filePath);
     const info = path.parse(filePath);
-    if (config.pathFilter && !config.pathFilter(filePath)) {
-      continue;
-    }
     if (
       stat.isFile() &&
       info.ext === ".json" &&
-      info.base !== config.translateCacheFileName
+      info.base !== config.translateCacheFileName &&
+      ((config.pathFilter && config.pathFilter(filePath)) || !config.pathFilter)
     ) {
       const data = fs.readFileSync(filePath);
       const swaggerData = JSON.parse(data.toString());
