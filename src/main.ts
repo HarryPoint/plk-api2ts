dcbeea3e0ee99a047a0c964b9a395bc5e0fd2a56
梁怀刚 <1751826104@qq.com>
2024-02-28 20:37:02 +0800

    feat: 调整命令

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import baseConfig, { IConfig } from "./config";
 import { main as createData } from "./createData";
 import { main as createTs } from "./createTs";
+const argv = require("yargs").argv;
 
 export const main = async (config: IConfig = baseConfig) => {
-  await createData(config);
-  await createTs(config);
+  if (!argv.type || argv.type === "createData") {
+    await createData(config);
+  }
+  if (!argv.type || argv.type === "createTs") {
+    await createTs(config);
+  }
 };
